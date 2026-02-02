72cb15107f7c435ee7b61751c6c0dc0a036d8b38
Bryan Hirawan <ryuukohirawan@gmail.com>
2026-02-02 19:33:24 +0900

    fix rust lsp

---
+++ after/plugin/rust.lua
@@ -0,0 +1,19 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "rust" },
+  callback = function()
+    vim.lsp.start({
+      name = "rust-analyzer",
+      cmd = { "rust-analyzer" },
+      root_dir = vim.fs.root(0, { "Cargo.toml", "rust-project.json", ".git" }),
+      settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = true,
+          check = {
+            command = "clippy", -- or "check" if you want faster
+          },
+          cargo = { allFeatures = true },
+        },
+      },
+    })
+  end,
+})
