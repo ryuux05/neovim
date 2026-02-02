d68dbacc03f4fef6c4d801ca7910a6bb7c11a82c
Bryan Hirawan <ryuukohirawan@gmail.com>
2026-02-03 08:13:35 +0900

    add previewer to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>pf', function() 
+    builtin.find_files({ previewer = true }) 
+end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
